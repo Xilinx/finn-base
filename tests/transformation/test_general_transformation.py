81dfee39d749ba72feccf40e43a95665d640ba2f
Lucian Petrica <lucianp@xilin.com>
2020-10-12 16:16:50 +0000

    Added ApplyConfig transform and test

--- tests/transformation/test_general_transformation.py
+++ tests/transformation/test_general_transformation.py
@@ -29,11 +29,14 @@
 import numpy as np
 import onnx
 from pkgutil import get_data
+import json
 
 import finn.core.onnx_exec as oxe
 from finn.core.modelwrapper import ModelWrapper
-from finn.transformation.general import GiveUniqueNodeNames, GiveUniqueParameterTensors
+from finn.transformation.general import GiveUniqueNodeNames, GiveUniqueParameterTensors, ApplyConfig
 from finn.transformation.infer_shapes import InferShapes
+from finn.transformation.lower_convs_to_matmul import LowerConvsToMatMul
+from finn.custom_op.registry import getCustomOp
 
 
 def test_give_unique_node_names():
@@ -119,3 +122,29 @@ def test_give_unique_parameter_tensors():
             param_cnt += 1
 
     assert len(param_set) == param_cnt, " There are still parameters reused"
+    
+    
+def test_apply_config():
+    raw_m = get_data("finn.data", "onnx/mnist-conv/model.onnx")
+    model = ModelWrapper(raw_m)
+    model = model.transform(GiveUniqueNodeNames())
+    model = model.transform(LowerConvsToMatMul())
+    model = model.transform(GiveUniqueNodeNames())
+    model.save("foo.onnx")
+    # set up a config in a dict, then dump it to JSON
+    config = {}
+    config["Defaults"] = {
+        "kernel_size":[3,["Im2Col"]]
+    }
+    config["Im2Col_0"] = {
+        "kernel_size":7
+    }
+    with open("config.json", "w") as f:
+        json.dump(config, f, indent=4)
+    # load json from file and apply to model
+    with open("config.json", "r") as f:
+        floorplan = json.load(f)
+    model = model.transform(ApplyConfig("config.json"))
+    # check model
+    assert getCustomOp(model.graph.node[2]).get_nodeattr("kernel_size") == 7
+    assert getCustomOp(model.graph.node[9]).get_nodeattr("kernel_size") == 3
