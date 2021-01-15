38815b6ab7953e1d50f32e81df4ea13c482ed2a2
Mirzam98 <mmrahorovic@hotmail.com>
2021-01-15 11:55:32 +0100

    [change_3d_tensors_to_4d]: added 3D to 4D transformation (for QuartzNet). [test_4d_conversion]: added test case for 3D to 4D transform.

--- src/finn/transformation/change_3d_tensors_to_4d.py
+++ src/finn/transformation/change_3d_tensors_to_4d.py
@@ -26,166 +26,67 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import numpy as np
 from finn.transformation.base import Transformation
-from finn.transformation.general import GiveReadableTensorNames, GiveUniqueNodeNames, RemoveUnusedTensors
+from finn.transformation.general import (
+    GiveReadableTensorNames,
+    GiveUniqueNodeNames,
+    RemoveUnusedTensors,
+)
 from finn.transformation.infer_shapes import InferShapes
+from finn.util.basic import get_by_name
 
-def find_invalid_nodes(graph):
-    """ Verifies whether the graph contains valid nodes """
-    valid_nodes = ["Add", "Mul", "BatchNormalization", "MultiThreshold", "Conv", "Transpose", "LogSoftmax", "ArgMax"]
+
+def find_invalid_nodes(model):
+    """
+    Verifies whether the graph contains valid nodes.
+    """
+    valid_nodes = [
+        "Add",
+        "Mul",
+        "BatchNormalization",
+        "MultiThreshold",
+        "Conv",
+        "Transpose",
+        "LogSoftmax",
+        "ArgMax",
+    ]
     invalid_nodes = []
-    for n in graph.node:
+    for n in model.graph.node:
         node_op_type = n.op_type
         if node_op_type in valid_nodes:
             continue
         else:
             invalid_nodes.append(node_op_type)
-    if not invalid_nodes: # if there are no invalid nodes
+    if not invalid_nodes:  # if there are no invalid nodes
         return True
     else:
-        raise Exception("Nodes {} are not supported in this transformation".format(invalid_nodes))
-
-
-def convert_3d_to_4d_tensors(model):
-    """ Converts 3D tensors (input, value_info, output) to 4D tensors """
-    tensor_names = {}
-    ## Inputs
-    for t in model.graph.input:
-        tensor_name = t.name
-        tensor_type = t.type.tensor_type.elem_type
-        tensor_shape = model.get_tensor_shape(tensor_name)
-        tensor_names[tensor_name] = [tensor_type]
-        tensor_names[tensor_name].append(tensor_shape)
-
-    ## Initializers
-    initializer_names = []
-    for i in model.graph.initializer:
-        initializer_names.append(i.name)
-
-    ## Value infos
-    for t in model.graph.value_info:
-        tensor_name = t.name
-        if tensor_name in initializer_names:
-            continue
-        else:
-            tensor_type = t.type.tensor_type.elem_type
-            tensor_shape = model.get_tensor_shape(tensor_name)
-            tensor_names[tensor_name] = [tensor_type]
-            tensor_names[tensor_name].append(tensor_shape)
-
-    ## Outputs
-    for t in model.graph.output:
-        tensor_name = t.name
-        tensor_type = t.type.tensor_type.elem_type
-        tensor_shape = model.get_tensor_shape(tensor_name)
-        tensor_names[tensor_name] = [tensor_type]
-        tensor_names[tensor_name].append(tensor_shape)
-
-    ## Find tensors that are the output of nodes that reduce the dimension
-    tensors_reduced_dimension = []
-    for n in model.graph.node:
-        if n.op_type=="ArgMax":
-            keep_dims = get_by_name(n.attribute, "keepdims", "name").i
-            if keep_dims == 0:
-                node_out = n.output
-                for n_o in node_out:
-                    tensors_reduced_dimension.append(n_o)
-
-    ## Change format of each value_info + input + output tensors
-    for k, v in tensor_names.items():
-        tensor_type = v[0]
-        shape = v[1]
-        ## Add extra dimension for tensors:
-        # 1) Have 3 dimensions (NCH -> NCH1)
-        # 2) Tensors following operations that reduce their dimension: {Argmax, ...}
-        if len(shape)==3 or k in tensors_reduced_dimension:
-            shape.append(1)
-            model.set_tensor_shape(k, shape, tensor_type)
-        else:
-            continue
-
-def make_node_4d_compatible(model):
-    """ Each node from the list of supported nodes is made compatible with 4D tensors"""
-    for n in model.graph.node:
-        node_op_type = n.op_type
-        if node_op_type == "Transpose":
-            perm = get_by_name(n.attribute,"perm", "name").ints
-            if len(perm) == 3: # Meaning transpose was on 3D tensor
-                perm.append(3) # append 4th dimension
-        elif node_op_type == "ArgMax" or node_op_type == "LogSoftMax":
-            axis = get_by_name(n.attribute, "axis", "name")
-            if axis.i == -1:
-                axis.i = 2 # argmax is now on the second-to-last axis
-        elif node_op_type == "Conv":
-            dilations = get_by_name(n.attribute, "dilations", "name").ints
-            kernel_shape = get_by_name(n.attribute, "kernel_shape", "name").ints
-            pads = get_by_name(n.attribute, "pads", "name").ints
-            strides = get_by_name(n.attribute, "strides", "name").ints
-            if len(dilations) == 1: # we must add another dimension to it
-                dilations.append(dilations[0]) # only equal dilation value along each spatial axis is supported
-            if len(kernel_shape) == 1: # we must add another dimension to it
-                kernel_shape.append(1)
-            if len(pads) == 2: # pads = ([x1_begin, x1_end] -->) [x1_begin, x2_begin, x1_end, x2_end]
-                pads.insert(1, 0)
-                pads.append(0)
-            if len(strides) == 1: # strides = [stride_H, stride_W]
-                strides.append(1)
-
-def create_4d_initializers_conv_mul_add_node(model):
-    """ Conv, Mul and Add nodes are made compatible with 4D input tensors """
-    initializers = {}
-    for i in model.graph.initializer:
-        init_name = i.name
-        if "Conv" in init_name:
-            init_dim = i.dims
-            init_dtype = i.data_type
-            initializers[init_name] = [init_dtype]
-            initializers[init_name].append(init_dim)
-        elif init_name[0:4] == "Mul_":
-            init_dim = i.dims
-            if len(i.dims) == 3:
-                init_dtype = i.data_type
-                initializers[init_name] = [init_dtype]
-                initializers[init_name].append(init_dim)
-        elif "Add" in init_name:
-            init_dim = i.dims
-            if len(i.dims) == 3:
-                init_dtype = i.data_type
-                initializers[init_name] = [init_dtype]
-                initializers[init_name].append(init_dim)
-        else:
-            continue
-
-    for k, v in initializers.items():
-        init_dtype = v[0]
-        init_shape = v[1]
-        if len(init_shape) == 3:
-            # Change shape NCH -> NCH1
-            init_shape.append(1)
-            shape_init = model.get_initializer(k).shape
-            model.set_tensor_shape(k, init_shape, init_dtype)
+        raise Exception(
+            "Nodes: {} are not supported in the 3D to 4D transformation".format(
+                invalid_nodes
+            )
+        )
 
 
 class Change3DTo4DTensors(Transformation):
     """
-    Replaces 3D tensors to 4D tensors assuming the following format: [N, C, H] -> [N, C, H, 1].
+    Replaces 3D tensors to 4D tensors assuming the following format:
+    [N, C, H] -> [N, C, H, 1].
     The attributes of a certain set of supported nodes are changed accordingly.
     """
 
     def apply(self, model):
-        graph = model.graph
         graph_modified = False
 
         assert find_invalid_nodes(model)
-        ### Infer the shapes of each tensor, remove unused tensors and give each tensor a readable name
+        # Infer the shapes of each tensor, remove unused tensors
+        # and give each tensor a readable name
         model = model.transform(InferShapes())
         model = model.transform(RemoveUnusedTensors())
         model = model.transform(GiveUniqueNodeNames())
         model = model.transform(GiveReadableTensorNames())
 
-        ### Converts 3D tensors (input, value_info, output) to 4D tensors
-        ## Inputs
+        # Converts 3D tensors (input, value_info, output) to 4D tensors
+        # Inputs
         tensor_names = {}
         for t in model.graph.input:
             tensor_name = t.name
@@ -194,9 +95,9 @@ class Change3DTo4DTensors(Transformation):
             tensor_names[tensor_name] = [tensor_type]
             tensor_names[tensor_name].append(tensor_shape)
 
-        ## Initializers
-        initializer_names = [] # list with all initializers
-        initializers = {} # list with initializers from Conv, Mul, and Add nodes
+        # Initializers
+        initializer_names = []  # list with all initializers
+        initializers = {}  # list with initializers from Conv, Mul, and Add nodes
         for i in model.graph.initializer:
             init_name = i.name
             initializer_names.append(init_name)
@@ -220,7 +121,7 @@ class Change3DTo4DTensors(Transformation):
             else:
                 continue
 
-        ## Value infos
+        # Value infos
         for t in model.graph.value_info:
             tensor_name = t.name
             if tensor_name in initializer_names:
@@ -231,7 +132,7 @@ class Change3DTo4DTensors(Transformation):
                 tensor_names[tensor_name] = [tensor_type]
                 tensor_names[tensor_name].append(tensor_shape)
 
-        ## Outputs
+        # Outputs
         for t in model.graph.output:
             tensor_name = t.name
             tensor_type = t.type.tensor_type.elem_type
@@ -242,64 +143,64 @@ class Change3DTo4DTensors(Transformation):
         tensors_reduced_dimension = []
         for n in model.graph.node:
             node_op_type = n.op_type
-            ## Find tensors that are the output of nodes that reduce the dimension
+            # Find tensors that are the output of nodes that reduce the dimension
             if node_op_type == "ArgMax":
                 keep_dims = get_by_name(n.attribute, "keepdims", "name").i
                 if keep_dims == 0:
                     node_out = n.output
                     for n_o in node_out:
                         tensors_reduced_dimension.append(n_o)
-            ### Each node from the list of supported nodes is made compatible with 4D tensors
+            # Each node from the list of supported nodes is made compatible
+            # with 4D tensors
             if node_op_type == "Transpose":
-                perm = get_by_name(n.attribute,"perm", "name").ints
-                if len(perm) == 3: # Meaning that the transpose operation was on a 3D tensor
-                    perm.append(3) # append 4th dimension
+                perm = get_by_name(n.attribute, "perm", "name").ints
+                if (
+                    len(perm) == 3
+                ):  # Meaning that the transpose operation was on a 3D tensor
+                    perm.append(3)  # append 4th dimension
             elif node_op_type == "ArgMax" or node_op_type == "LogSoftMax":
                 axis = get_by_name(n.attribute, "axis", "name")
                 if axis.i == -1:
-                    axis.i = 2 # argmax is now on the second-to-last axis
+                    axis.i = 2  # argmax is now on the second-to-last axis
             elif node_op_type == "Conv":
                 dilations = get_by_name(n.attribute, "dilations", "name").ints
                 kernel_shape = get_by_name(n.attribute, "kernel_shape", "name").ints
                 pads = get_by_name(n.attribute, "pads", "name").ints
                 strides = get_by_name(n.attribute, "strides", "name").ints
-                if len(dilations) == 1: # we must add another dimension to it
-                    dilations.append(dilations[0]) # only equal dilation value along each spatial axis is supported
-                if len(kernel_shape) == 1: # we must add another dimension to it
+                if len(dilations) == 1:  # we must add another dimension to it
+                    dilations.append(
+                        dilations[0]
+                    )  # only equal dilation value along each spatial axis is supported
+                if len(kernel_shape) == 1:  # we must add another dimension to it
                     kernel_shape.append(1)
-                if len(pads) == 2: # pads = ([x1_begin, x1_end] -->) [x1_begin, x2_begin, x1_end, x2_end]
+                if (
+                    len(pads) == 2
+                ):  # pads = [x1_begin, x1_end] --> [x1_begin, x2_begin, x1_end, x2_end]
                     pads.insert(1, 0)
                     pads.append(0)
-                if len(strides) == 1: # strides = [stride_H, stride_W]
+                if len(strides) == 1:  # strides = [stride_H, stride_W]
                     strides.append(1)
 
-
-        ## Change format of each value_info + input + output tensors
+        # Change format of each value_info + input + output tensors
         for k, v in tensor_names.items():
             tensor_type = v[0]
             shape = v[1]
-            ## Add extra dimension for tensors:
+            # Add extra dimension for tensors that:
             # 1) Have 3 dimensions (NCH -> NCH1)
-            # 2) Tensors following operations that reduce their dimension: {Argmax, ...}
-            if len(shape)==3 or k in tensors_reduced_dimension:
+            # 2) Come after operations that reduce their dimension: e.g. {Argmax, ...}
+            if len(shape) == 3 or k in tensors_reduced_dimension:
                 shape.append(1)
                 model.set_tensor_shape(k, shape, tensor_type)
             else:
                 continue
 
-        ### Conv, Mul and Add nodes are made compatible with 4D input tensors
+        # Conv, Mul and Add nodes are made compatible with 4D input tensors
         for k, v in initializers.items():
             init_dtype = v[0]
             init_shape = v[1]
             if len(init_shape) == 3:
                 # Change shape NCH -> NCH1
                 init_shape.append(1)
-                shape_init = model.get_initializer(k).shape
                 model.set_tensor_shape(k, init_shape, init_dtype)
 
-
         return (model, graph_modified)
-
-        # convert_3d_to_4d_tensors(model)
-        # make_node_4d_compatible(model)
-        # create_4d_initializers_conv_mul_add_node(model)
--- tests/transformation/test_4d_conversion.py
+++ tests/transformation/test_4d_conversion.py
@@ -1,35 +1,376 @@
 import numpy as np
+import onnx
 
 import finn.core.onnx_exec as oxe
+from finn.core.datatype import DataType
 from finn.core.modelwrapper import ModelWrapper
 from finn.transformation.change_3d_tensors_to_4d import Change3DTo4DTensors
+from finn.util.basic import gen_finn_dt_tensor
 
-def create_unit_input(model):
-    pass
 
-def execute_unit_input_test(model):
-    pass
+def generate_random_input(model):
+    """
+    Creates input dictionary with a random numpy array
+    that matches the input tensor shape.
+    """
+    input_dict = {}
+    for i in range(len(model.graph.input)):
+        input_node = model.graph.input[i]
+        input_node_name = input_node.name
+        input_node_shape = model.get_tensor_shape(input_node_name)
+        i_val = gen_finn_dt_tensor(DataType.FLOAT32, input_node_shape)
+        input_dict[input_node_name] = i_val
+    return input_dict
+
+
+def set_all_initializers(model):
+    """ Sets all initializers of the graph to a random value. """
+    for n in model.graph.node:
+        if len(n.input) > 1:
+            init_name = n.input[1]
+            init_shape = model.get_tensor_shape(init_name)
+            init_val = gen_finn_dt_tensor(DataType.FLOAT32, init_shape)
+            model.set_initializer(init_name, init_val)
+
+
+def create_arbitrary_model(invalid=False):
+    """
+    Creates arbitrary model for testing the 3D to 4D transform.
+    This model is based on a sub part of QuartzNet.
+    """
+    if invalid is False:
+        Mul1_node = onnx.helper.make_node(
+            "Mul",
+            inputs=["in1_mul1", "in2_mul1"],  # inputs
+            outputs=["out1_mul1"],  # outputs
+            name="Mul1",  # name
+        )
+
+        Conv1_node = onnx.helper.make_node(
+            "Conv",
+            inputs=["out1_mul1", "in2_conv1"],
+            outputs=["out1_conv1"],
+            name="Conv1",
+            dilations=[1],
+            group=1,
+            kernel_shape=[1],
+            pads=[0, 0],
+            strides=[1],
+        )
+
+        Add1_node = onnx.helper.make_node(
+            "Add", inputs=["out1_conv1", "in2_add1"], outputs=["out1_add1"], name="Add1"
+        )
+
+        Mul2_node = onnx.helper.make_node(
+            "Mul", inputs=["out1_add1", "in2_mul2"], outputs=["out1_mul2"], name="Mul2"
+        )
+
+        Transpose1_node = onnx.helper.make_node(
+            "Transpose",
+            inputs=["out1_mul2"],
+            outputs=["out1_transpose1"],
+            name="Transpose1",
+            perm=[0, 2, 1],
+        )
+
+        LogSoftmax1_node = onnx.helper.make_node(
+            "LogSoftmax",
+            inputs=["out1_transpose1"],
+            outputs=["out1_logsoftmax1"],
+            name="LogSoftmax1",
+            axis=2,
+        )
+
+        ArgMax1_node = onnx.helper.make_node(
+            "ArgMax",
+            inputs=["out1_logsoftmax1"],
+            outputs=["out1_argmax1"],
+            name="ArgMax1",
+            axis=-1,
+            keepdims=0,
+        )
+
+        # Inputs and outputs
+        in1_mul1 = onnx.helper.make_tensor_value_info(
+            "in1_mul1", onnx.TensorProto.FLOAT, [1, 1024, 128]
+        )
+        out1_argmax1 = onnx.helper.make_tensor_value_info(
+            "out1_argmax1", onnx.TensorProto.INT64, [1, 128]
+        )
+
+        # Value infos
+        out1_mul1 = onnx.helper.make_tensor_value_info(
+            "out1_mul1", onnx.TensorProto.FLOAT, [1, 1024, 128]
+        )
+        out1_conv1 = onnx.helper.make_tensor_value_info(
+            "out1_conv1", onnx.TensorProto.FLOAT, [1, 29, 128]
+        )
+        out1_add1 = onnx.helper.make_tensor_value_info(
+            "out1_add1", onnx.TensorProto.FLOAT, [1, 29, 128]
+        )
+        out1_mul2 = onnx.helper.make_tensor_value_info(
+            "out1_mul2", onnx.TensorProto.FLOAT, [1, 29, 128]
+        )
+        out1_transpose1 = onnx.helper.make_tensor_value_info(
+            "out1_transpose1", onnx.TensorProto.FLOAT, [1, 128, 29]
+        )
+        out1_logsoftmax1 = onnx.helper.make_tensor_value_info(
+            "out1_logsoftmax1", onnx.TensorProto.FLOAT, [1, 128, 29]
+        )
+
+        # Initializers
+        in2_mul1 = onnx.helper.make_tensor_value_info(
+            "in2_mul1", onnx.TensorProto.FLOAT, [1]
+        )
+        in2_conv1 = onnx.helper.make_tensor_value_info(
+            "in2_conv1", onnx.TensorProto.FLOAT, [29, 1024, 1]
+        )
+        in2_add1 = onnx.helper.make_tensor_value_info(
+            "in2_add1", onnx.TensorProto.FLOAT, [1, 29, 1]
+        )
+        in2_mul2 = onnx.helper.make_tensor_value_info(
+            "in2_mul2", onnx.TensorProto.FLOAT, [1]
+        )
+
+        graph = onnx.helper.make_graph(
+            nodes=[
+                Mul1_node,
+                Conv1_node,
+                Add1_node,
+                Mul2_node,
+                Transpose1_node,
+                LogSoftmax1_node,
+                ArgMax1_node,
+            ],
+            name="4d_conversion_test_graph",
+            inputs=[in1_mul1],
+            outputs=[out1_argmax1],
+            value_info=[
+                out1_mul1,
+                out1_conv1,
+                out1_add1,
+                out1_mul2,
+                out1_transpose1,
+                out1_logsoftmax1,
+                in2_mul1,
+                in2_conv1,
+                in2_add1,
+                in2_mul2,
+            ],
+        )
+        onnx_model = onnx.helper.make_model(
+            graph, producer_name="4d_conversion_test-model"
+        )
+        model = ModelWrapper(onnx_model)
+        return model
+
+    else:  # In this scenario, a Relu node is added after the Conv node
+        Mul1_node = onnx.helper.make_node(
+            "Mul",
+            inputs=["in1_mul1", "in2_mul1"],  # inputs
+            outputs=["out1_mul1"],  # outputs
+            name="Mul1",  # name
+        )
+
+        Conv1_node = onnx.helper.make_node(
+            "Conv",
+            inputs=["out1_mul1", "in2_conv1"],
+            outputs=["out1_conv1"],
+            name="Conv1",
+            dilations=[1],
+            group=1,
+            kernel_shape=[1],
+            pads=[0, 0],
+            strides=[1],
+        )
+
+        Relu1_node = onnx.helper.make_node(
+            "Relu", inputs=["out1_conv1"], outputs=["out1_relu1"], name="Relu1"
+        )
+
+        Add1_node = onnx.helper.make_node(
+            "Add", inputs=["out1_relu1", "in2_add1"], outputs=["out1_add1"], name="Add1"
+        )
+
+        Mul2_node = onnx.helper.make_node(
+            "Mul", inputs=["out1_add1", "in2_mul2"], outputs=["out1_mul2"], name="Mul2"
+        )
+
+        Transpose1_node = onnx.helper.make_node(
+            "Transpose",
+            inputs=["out1_mul2"],
+            outputs=["out1_transpose1"],
+            name="Transpose1",
+            perm=[0, 2, 1],
+        )
+
+        LogSoftmax1_node = onnx.helper.make_node(
+            "LogSoftmax",
+            inputs=["out1_transpose1"],
+            outputs=["out1_logsoftmax1"],
+            name="LogSoftmax1",
+            axis=2,
+        )
+
+        ArgMax1_node = onnx.helper.make_node(
+            "ArgMax",
+            inputs=["out1_logsoftmax1"],
+            outputs=["out1_argmax1"],
+            name="ArgMax1",
+            axis=-1,
+            keepdims=0,
+        )
+
+        # Inputs and outputs
+        in1_mul1 = onnx.helper.make_tensor_value_info(
+            "in1_mul1", onnx.TensorProto.FLOAT, [1, 1024, 128]
+        )
+        out1_argmax1 = onnx.helper.make_tensor_value_info(
+            "out1_argmax1", onnx.TensorProto.INT64, [1, 128]
+        )
+
+        # Value infos
+        out1_mul1 = onnx.helper.make_tensor_value_info(
+            "out1_mul1", onnx.TensorProto.FLOAT, [1, 1024, 128]
+        )
+        out1_conv1 = onnx.helper.make_tensor_value_info(
+            "out1_conv1", onnx.TensorProto.FLOAT, [1, 29, 128]
+        )
+        out1_relu1 = onnx.helper.make_tensor_value_info(
+            "out1_relu1", onnx.TensorProto.FLOAT, [1, 29, 128]
+        )
+        out1_add1 = onnx.helper.make_tensor_value_info(
+            "out1_add1", onnx.TensorProto.FLOAT, [1, 29, 128]
+        )
+        out1_mul2 = onnx.helper.make_tensor_value_info(
+            "out1_mul2", onnx.TensorProto.FLOAT, [1, 29, 128]
+        )
+        out1_transpose1 = onnx.helper.make_tensor_value_info(
+            "out1_transpose1", onnx.TensorProto.FLOAT, [1, 128, 29]
+        )
+        out1_logsoftmax1 = onnx.helper.make_tensor_value_info(
+            "out1_logsoftmax1", onnx.TensorProto.FLOAT, [1, 128, 29]
+        )
+
+        # Initializers
+        in2_mul1 = onnx.helper.make_tensor_value_info(
+            "in2_mul1", onnx.TensorProto.FLOAT, [1]
+        )
+        in2_conv1 = onnx.helper.make_tensor_value_info(
+            "in2_conv1", onnx.TensorProto.FLOAT, [29, 1024, 1]
+        )
+        in2_add1 = onnx.helper.make_tensor_value_info(
+            "in2_add1", onnx.TensorProto.FLOAT, [1, 29, 1]
+        )
+        in2_mul2 = onnx.helper.make_tensor_value_info(
+            "in2_mul2", onnx.TensorProto.FLOAT, [1]
+        )
+
+        graph = onnx.helper.make_graph(
+            nodes=[
+                Mul1_node,
+                Conv1_node,
+                Relu1_node,
+                Add1_node,
+                Mul2_node,
+                Transpose1_node,
+                LogSoftmax1_node,
+                ArgMax1_node,
+            ],
+            name="4d_conversion_test_graph",
+            inputs=[in1_mul1],
+            outputs=[out1_argmax1],
+            value_info=[
+                out1_mul1,
+                out1_conv1,
+                out1_relu1,
+                out1_add1,
+                out1_mul2,
+                out1_transpose1,
+                out1_logsoftmax1,
+                in2_mul1,
+                in2_conv1,
+                in2_add1,
+                in2_mul2,
+            ],
+        )
+        onnx_model = onnx.helper.make_model(
+            graph, producer_name="4d_conversion_test-model"
+        )
+        model = ModelWrapper(onnx_model)
+        return model
+
 
 def test_4d_conversion():
-    ## Create model
-    # Extract part of QuartzNet and save is in onnx format:
+    """
+    Test for the 3D to 4D transformation with a valid graph.
+    """
+    model = create_arbitrary_model(invalid=False)
 
-    ## Call transformation
-    # model = model.transform(Change3DTo4DTensors())
+    # Inputs
+    input_dict = generate_random_input(model)
 
-    ## Create input
-    # create_unit_input(model)
+    # Initializers
+    set_all_initializers(model)
 
-    ## Compare outputs
-    # 1) original model (unmodified - part extracted in Create Model)
-    # 2) modified model (after calling the 3D->4D transformation)
-    pass
+    # Comparing the outputs of the model before and after the transform
+    output_node_name = model.graph.output[0].name
+    output_dict = oxe.execute_onnx(model, input_dict, return_full_exec_context=True)
+    expected = output_dict[output_node_name]
+
+    model = model.transform(Change3DTo4DTensors())
+
+    for k, v in input_dict.items():
+        old_in_name = k
+        old_shape = np.shape(v)
+        new_in_name = model.graph.input[0].name
+        new_shape = old_shape + (1,)
+    new_in_val = np.reshape(v, new_shape)
+    del input_dict[old_in_name]
+    input_dict[new_in_name] = new_in_val
+
+    output_node_name = model.graph.output[0].name
+    output_dict = oxe.execute_onnx(model, input_dict, return_full_exec_context=True)
+    expected_modified = output_dict[output_node_name]
+
+    expected_modified = np.reshape(expected_modified, np.shape(expected))
+
+    assert (expected == expected_modified).all()
 
 
 def test_4d_conversion_invalid_nodes():
-    pass
+    """
+    Test for the 3D to 4D transformation when invalid graph is supplied.
+    """
+    model = create_arbitrary_model(invalid=True)
 
+    # Inputs
+    input_dict = generate_random_input(model)
 
-#### TO DO
-# Extract model from QuartzNet
-# Fill in the steps above and verify functionality
+    # Initializers
+    set_all_initializers(model)
+
+    # Comparing the outputs of the model before and after the transform
+    output_node_name = model.graph.output[0].name
+    output_dict = oxe.execute_onnx(model, input_dict, return_full_exec_context=True)
+    expected = output_dict[output_node_name]
+
+    model = model.transform(Change3DTo4DTensors())
+
+    for k, v in input_dict.items():
+        old_in_name = k
+        old_shape = np.shape(v)
+        new_in_name = model.graph.input[0].name
+        new_shape = old_shape + (1,)
+    new_in_val = np.reshape(v, new_shape)
+    del input_dict[old_in_name]
+    input_dict[new_in_name] = new_in_val
+
+    output_node_name = model.graph.output[0].name
+    output_dict = oxe.execute_onnx(model, input_dict, return_full_exec_context=True)
+    expected_modified = output_dict[output_node_name]
+
+    expected_modified = np.reshape(expected_modified, np.shape(expected))
+
+    assert (expected == expected_modified).all()
