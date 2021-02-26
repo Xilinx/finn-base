5ec25823179e2f086bd6aed43cae16757ced50a2
jalezeta <jonanderlezeta@gmail.com>
2021-02-26 20:08:44 +0000

    Adding zero entries in the custom operations, attributes, and a transformation to generate a Verilog truth table based on the table entries.

--- tests/custom_op/test_truthtable.py
+++ tests/custom_op/test_truthtable.py
@@ -33,6 +33,7 @@ from onnx import TensorProto
 import finn.core.onnx_exec as oxe
 from finn.core.datatype import DataType
 from finn.core.modelwrapper import ModelWrapper
+from finn.transformation.gen_verilog_truth import GenVerilogTruthTable
 from finn.transformation.infer_datatypes import InferDataTypes
 from finn.transformation.infer_shapes import InferShapes
 
@@ -42,39 +43,54 @@ export_onnx_path = "test_truthtable.onnx"
 def test_truthtable():
 
     input_data = np.asarray([1, 0, 0, 1, 1, 0, 0, 0, 1, 1], dtype=np.float32)
-    results_data = np.asarray([5, 8, 14, 198, 611], dtype=np.float32)
+    result_one_data = np.asarray([58, 15, 89, 695, 6485], dtype=np.float32)
+    result_zero_data = np.asarray([52, 65, 1908, 6101], dtype=np.float32)
+    dont_care = 0
+    in_bits = 16
 
     inputs = helper.make_tensor_value_info(
-        "inputs", TensorProto.FLOAT, [10]
-    )  # Input bitwidth 10
-    results = helper.make_tensor_value_info(
-        "results", TensorProto.FLOAT, [5]
-    )  # 5 results are 1 among all possible combinations
+        "inputs", TensorProto.FLOAT, [input_data.size]
+    )
+    result_one = helper.make_tensor_value_info(
+        "result_one", TensorProto.FLOAT, [result_one_data.size]
+    )
+    result_zero = helper.make_tensor_value_info(
+        "result_zero", TensorProto.FLOAT, [result_zero_data.size]
+    )
     output = helper.make_tensor_value_info("output", TensorProto.FLOAT, [1])
 
     node_def = helper.make_node(
         "TruthTable",
-        ["inputs", "results"],
+        ["inputs", "result_one", "result_zero"],
         ["output"],
         domain="finn.custom_op.general",
-        in_bits=input_data.size,
+        in_bits=in_bits,
+        dont_care=dont_care,
     )
     modelproto = helper.make_model(
-        helper.make_graph([node_def], "test_model", [inputs, results], [output])
+        helper.make_graph(
+            [node_def], "test_model", [inputs, result_one, result_zero], [output]
+        )
     )
 
     model = ModelWrapper(modelproto)
     model.set_tensor_datatype("inputs", DataType.BINARY)
-    model.set_tensor_datatype("results", DataType.UINT32)
+    model.set_tensor_datatype("result_one", DataType.UINT32)
+    model.set_tensor_datatype("result_zero", DataType.UINT32)
+
     # test output shape
     model = model.transform(InferShapes())
     assert model.get_tensor_shape("output") == [1]
     # test output type
     assert model.get_tensor_datatype("output") is DataType.FLOAT32
     model = model.transform(InferDataTypes())
-    assert model.get_tensor_datatype("output") is DataType.BINARY
+    assert model.get_tensor_datatype("output") is DataType.UINT32
     # perform execution
-    in_dict = {"inputs": input_data, "results": results_data}
+    in_dict = {
+        "inputs": input_data,
+        "result_one": result_one_data,
+        "result_zero": result_zero_data,
+    }
     out_dict = oxe.execute_onnx(model, in_dict)
 
     # calculate result here for comparison with the custom op
@@ -82,7 +98,17 @@ def test_truthtable():
     out_idx = 0
     for idx, val in enumerate(input_data):
         out_idx += (1 << idx) * val
-    entry = 1 if out_idx in results_data else 0
+    entry = (
+        1
+        if out_idx in result_one_data
+        else (0 if out_idx in result_zero_data else dont_care)
+    )
 
     # compare outputs
     assert entry == out_dict["output"]
+    # test transformation to generate verilog
+    model = model.transform(
+        GenVerilogTruthTable(
+            num_workers=None, result_one=result_one_data, result_zero=result_zero_data
+        )
+    )
