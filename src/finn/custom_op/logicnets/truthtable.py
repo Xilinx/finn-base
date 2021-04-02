32fef3338e0cd82bb3c29ad9703db66b249bf98a
Jon Ander Lezeta <jonanderlezeta@gmail.com>
2021-04-02 15:22:34 +0100

    1- Add TruthTable customOp for X:Y LUT operations(python and rtlsim).\n2- Add node level Verilog generation transformation. \n3- Add tests to verify functionalities.

--- src/finn/custom_op/logicnets/truthtable.py
+++ src/finn/custom_op/logicnets/truthtable.py
@@ -36,64 +36,86 @@ from finn.core.datatype import DataType
 from finn.core.modelwrapper import ModelWrapper
 from finn.custom_op.base import CustomOp
 from finn.util.basic import make_build_dir
-from finn.util.data_packing import npy_to_rtlsim_input
+from finn.util.data_packing import (
+    npy_to_rtlsim_input,
+    unpack_innermost_dim_from_hex_string,
+)
 
 
-def binary_truthtable(input, care_set, bits):
+def _truthtable(input, care_set, results, in_bits):
     """Returns the output to a combination of x-bit input value. The care_set array
-    reflects the true values in the truth table results. The rest of the entries are
-    just zero or dont-cares. If 2 is provided in the care-set, the result to fifth
-    combination of inputs 010 is 1. The input is a vector size x, representing
-    x-bits binary input.
+    reflects the specific input combinations considered. The result vector represents
+    every output to every combination in the care_set. Thus, the length of care_set
+    and results must be the same. All the arrays are numpy arrays
 
     **************************************************************************
     The MSB in the input numpy array represents the LSB in the LUT.
     **************************************************************************
 
     An example is presented:
+    in_bits = 3
+    out_bits = 3
+    input[0:2] = [1, 0, 1]
+    care_set = [1, 5]
+    results = [3, 1]
 
-    inputs[0:2] = [1, 0, 1]
-    care_set = [1, 2]
+    The function checks if the decimal representation of the binary input is in
+    the care_set. If it is in the care_set, we check the result of the binary
+    input combination in the result. If the input is not part of the care_set,
+    the output will be zero.
 
-    Possible combinations[2:0]:      A   B   C  | Results
-                                    ---------------------
-                                    0   0   0   |    0
-                                    0   0   1   |    1
-                                    0   1   0   |    1
-                                    0   1   1   |    0
-                                    1   0   0   |    0
-                                    1   0   1   |    0
-                                    1   1   0   |    0
-                                    1   1   1   |    0
+    The input in this example is [1,0,1], which is '5' in decimal representation.
+    The input is part of the care_set. Then, we check what is the position of 5
+    in the care_set, and we extract the value to input combination '5' from the
+    results vector, which in this case is '1'
+
+    Possible combinations[2:0]:     input[0:2]  |  results[0:2]
+                                   -------------------------------
+                                    0   0   0   |    0    0    0
+                                    0   0   1   |    0    1    1
+                                    0   1   0   |    0    0    0
+                                    0   1   1   |    0    0    0
+                                  > 1   0   0   |    0    0    1
+                                    1   0   1   |    0    0    0
+                                    1   1   0   |    0    0    0
+                                    1   1   1   |    0    0    0
 
     """
 
     # calculate integer value of binary input
-    in_int = npy_to_rtlsim_input(input, DataType.BINARY, bits, False)[0]
-    # return 1 if the input is in result_one
-    output = 1 if in_int in care_set else 0
+    input_int = npy_to_rtlsim_input(input, DataType.BINARY, in_bits, False)[0]
+    if input_int in care_set:
+        index = np.where(care_set == input_int)[0][0]
+        output = results[index]
+    else:
+        output = 0
 
     return output
 
 
-class BinaryTruthTable(CustomOp):
+class TruthTable(CustomOp):
     """The class corresponing to the Binary TruthTable function. """
 
     def get_nodeattr_types(self):
         return {
-            # number of intput bits, 2 by default
-            "in_bits": ("i", True, 2),
+            # number of intput bits, 4 by default
+            "in_bits": ("i", True, 4),
+            # number of output bits, 4 by default
+            "out_bits": ("i", True, 4),
             # code generation mode
             "code_mode": ("s", False, "Verilog"),
             # output code directory
             "code_dir": ("s", False, ""),
-            # execution mode, "pyhton" by default
+            # execution mode, "python" by default
             "exec_mode": ("s", True, "python"),
         }
 
     def make_shape_compatible_op(self, model):
         node = self.onnx_node
-        val = np.random.randn(1).astype(np.bool)
+        out_bits = self.get_nodeattr("out_bits")
+
+        val = np.random.randint(2, size=out_bits)
+
         tensor_name = ModelWrapper.make_new_valueinfo_name(model)
         node = helper.make_node(
             "Constant",
@@ -118,45 +140,78 @@ class BinaryTruthTable(CustomOp):
         assert (
             model.get_tensor_datatype(node.input[1]) == DataType["UINT32"]
         ), """ The input vector DataType is not UINT32."""
+        # check that the input[2] is UINT32
+        assert (
+            model.get_tensor_datatype(node.input[2]) == DataType["UINT32"]
+        ), """ The input vector DataType is not UINT32."""
         # set the output[0] tensor datatype to BINARY
         model.set_tensor_datatype(node.output[0], DataType["BINARY"])
 
     def execute_node(self, context, graph):
         node = self.onnx_node
-        # load inputs
+        # Load inputs
+        # We assume input[0] is the input_vector, input[1] the care_set
+        # and the input[2] the results
         input_entry = context[node.input[0]]
         care_set = context[node.input[1]]
+        results = context[node.input[2]]
+        # VERIFICATION: care_set and results tensor shape and sizes
+        #
         # check input_entry size
         in_size = input_entry.size
-        expected_in_size = self.get_nodeattr("in_bits")
+        in_bits = self.get_nodeattr("in_bits")
+        out_bits = self.get_nodeattr("out_bits")
         assert (
-            in_size == expected_in_size
+            in_size == in_bits
         ), """The input bit array vector is %i and should be %i""" % (
             in_size,
-            expected_in_size,
+            in_bits,
         )
+        # check the maximum value of care_set values is smaller than 2^in_bits
+        max_care_set = np.amax(care_set)
+        max_in = 1 << in_bits
+        assert max_care_set < max_in
+        # check the maximum value of the results is smaller than 2^out_bits
+        max_results = np.amax(results)
+        max_out = 1 << out_bits
+        assert max_results < max_out
         # check input_entry shape
         assert (
             len(input_entry.shape) == 1
         ), """The input vector has more than one dimension."""
+        # check care_set shape
+        assert (
+            len(care_set.shape) == 1
+        ), """The care_set vector has more than one dimension."""
+        # check results shape
+        assert (
+            len(results.shape) == 1
+        ), """The results vector has more than one dimension."""
+        # check the care_set and results sizes are the same
+        assert (
+            care_set.size == results.size
+        ), """The care_set size is %s and results size is %s. Must
+            be the same """ % (
+            care_set.size,
+            results.size,
+        )
         # load execution mode
         mode = self.get_nodeattr("exec_mode")
         if mode == "python":
             # calculate output in Python mode
-            output = binary_truthtable(
-                input_entry, care_set, self.get_nodeattr("in_bits")
-            )
+            output = _truthtable(input_entry, care_set, results, in_bits)
         elif mode == "rtlsim":
             # check the code directory is not empty
             nodeName = self.onnx_node.name
-            verilog_dir = self.get_nodeattr("code_dir") + "/" + nodeName + ".v"
+            code_dir = self.get_nodeattr("code_dir")
+            verilog_dir = code_dir + "/" + nodeName + ".v"
             if not os.path.exists(verilog_dir):
                 raise Exception("Non valid path for the Verilog file: %s" % verilog_dir)
+            # Create PyVerilator object
             sim = PyVerilator.build(verilog_dir)
-            bits = self.get_nodeattr("in_bits")
-            # convert input binary float array into an integer
-            value = npy_to_rtlsim_input(input_entry, DataType.BINARY, bits, False)[0]
-            # set value into the Verilog module
+            # Convert input binary array into an integer representation
+            value = npy_to_rtlsim_input(input_entry, DataType.BINARY, in_bits, False)[0]
+            # Set input value into the Verilog module
             sim.io["in"] = value
             # read result value
             output = sim.io["result"]
@@ -167,14 +222,18 @@ class BinaryTruthTable(CustomOp):
                     mode
                 )
             )
-        # return output
-        context[node.output[0]] = np.array([output], dtype=np.float32)
+        # return output and convert it back into a binary array
+        output_hex = np.array([hex(int(output))])
+        out_array = unpack_innermost_dim_from_hex_string(
+            output_hex, DataType.BINARY, (out_bits,), out_bits
+        )
+        context[node.output[0]] = out_array
 
     def verify_node(self):
         info_messages = []
 
         # verify number of attributes
-        num_of_attr = 4
+        num_of_attr = 5
         if len(self.onnx_node.attribute) == num_of_attr:
             info_messages.append("The number of attributes is correct")
         else:
@@ -188,49 +247,83 @@ class BinaryTruthTable(CustomOp):
         # verify that all necessary attributes exist
         try:
             self.get_nodeattr("in_bits")
+            self.get_nodeattr("out_bits")
             self.get_nodeattr("exec_mode")
         except Exception:
             info_messages.append(
                 """The required attributes are not
-                set. BinaryTruthTable operation reqires in_bits and
-                exec_mode attributes."""
+                set. BinaryTruthTable operation reqires in_bits,
+                out_bits andb exec_mode attributes."""
             )
 
         # verify the number of inputs
-        if len(self.onnx_node.input) == 2:
+        if len(self.onnx_node.input) == 3:
             info_messages.append("The number of inputs is correct")
         else:
-            info_messages.append("BinaryTruthTable needs 2 data inputs")
+            info_messages.append("BinaryTruthTable needs 3 data inputs")
 
         return info_messages
 
-    def generate_verilog(self, care_set):
+    def generate_verilog(self, care_set, results):
 
         input_bits = self.get_nodeattr("in_bits")
+        output_bits = self.get_nodeattr("out_bits")
         nodeName = self.onnx_node.name
+        # VERIFICATION: care_set and results tensor shape and sizes
+        #
+        # check the maximum value of care_set values is smaller than 2^in_bits
+        max_care_set = np.amax(care_set)
+        max_in = 1 << input_bits
+        assert max_care_set < max_in
+        # check the maximum value of the results is smaller than 2^out_bits
+        max_results = np.amax(results)
+        max_out = 1 << output_bits
+        assert max_results < max_out
+        # check care_set shape
+        assert (
+            len(care_set.shape) == 1
+        ), """The care_set vector has more than one dimension."""
+        # check results shape
+        assert (
+            len(results.shape) == 1
+        ), """The results vector has more than one dimension."""
+        # check the care_set and results sizes are the same
+        care_set_size = care_set.size
+        results_size = results.size
+        assert (
+            care_set_size == results_size
+        ), """The care_set size is %s and results size is %s. Must
+            be the same """ % (
+            care_set_size,
+            results_size,
+        )
         # the module name is kept constant to "incomplete_table"
         # the input name is kept constant to "in"
         # the output is kept constant to "result"
         verilog_string = "module %s (\n" % (nodeName)
         verilog_string += "\tinput [%d:0] in,\n" % (input_bits - 1)
-        verilog_string += "\t output reg result\n"
+        verilog_string += "\t output reg [%d:0] result\n" % (output_bits - 1)
         verilog_string += ");\n\n"
         verilog_string += "\talways @(in) begin\n"
         verilog_string += "\t\tcase(in)\n"
 
         # fill the one entries
-        for val in care_set:
+        for index, val in enumerate(care_set):
             val = int(val)
             verilog_string += "\t\t\t%d'b" % (input_bits)
             verilog_string += bin(val)[2:].zfill(input_bits)
-            verilog_string += " : result = 1'b1;\n"
+            verilog_string += " : result = %d'b" % (output_bits)
+            verilog_string += bin(results[index])[2:].zfill(output_bits)
+            verilog_string += ";\n"
 
         # fill the rest of the combinations with 0
-        verilog_string += "\t\t\tdefault: result = 1'b0;\n"
+        verilog_string += "\t\t\tdefault: result = %d'b" % (output_bits)
+        verilog_string += bin(0)[2:].zfill(output_bits)
+        verilog_string += ";\n"
         # close the module
         verilog_string += "\t\tendcase\n\tend\nendmodule\n"
         # create temporary folder and save attribute value
-        self.set_nodeattr("code_dir", make_build_dir("BinaryTruthTable_verilog_"))
+        self.set_nodeattr("code_dir", make_build_dir("TruthTable_verilog_"))
         # create and write verilog file
         verilog_file = open(self.get_nodeattr("code_dir") + "/" + nodeName + ".v", "w")
         verilog_file.write(verilog_string)
--- tests/custom_op/test_truthtable.py
+++ tests/custom_op/test_truthtable.py
@@ -26,6 +26,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+
 import numpy as np
 import onnx.helper as helper
 from onnx import TensorProto
@@ -37,15 +38,11 @@ from finn.custom_op.registry import getCustomOp
 from finn.transformation.general import GiveUniqueNodeNames
 from finn.transformation.infer_datatypes import InferDataTypes
 from finn.transformation.infer_shapes import InferShapes
-from finn.transformation.logicnets.gen_bintruthtable_verilog import (
-    GenBinaryTruthTableVerilog,
-)
+from finn.transformation.logicnets.gen_truthtable_verilog import GenTruthTableVerilog
 from finn.util.data_packing import npy_to_rtlsim_input
 
-# export_onnx_path = "test_truthtable.onnx"
 
-
-def test_binarytruthtable():
+def test_truthtable():
 
     # Tensor with different input combinations
     input_data_vector = np.array(
@@ -56,55 +53,68 @@ def test_binarytruthtable():
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1],
             [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1],
             [0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        ],
-        dtype=np.float32,
+        ]
     )
-    # Set the care set
-    care_set_data = np.asarray([1, 58, 15, 89, 695, 6485], dtype=np.float32)
-    in_bits = 16
 
+    # Set the care set
+    care_set_data = np.asarray([1, 58, 15, 89, 695, 6485])
+    in_bits = 16
+    out_bits = 14
+    results_data = np.asarray([5, 8473, 382, 8774, 9494, 9322])
     # Set input and output tensor information
-    inputs = helper.make_tensor_value_info("inputs", TensorProto.FLOAT, [in_bits])
+    in_val = helper.make_tensor_value_info("in_val", TensorProto.FLOAT, [in_bits])
     care_set = helper.make_tensor_value_info(
         "care_set", TensorProto.FLOAT, [care_set_data.size]
     )
-    output = helper.make_tensor_value_info("output", TensorProto.FLOAT, [1])
+    results = helper.make_tensor_value_info(
+        "results", TensorProto.FLOAT, [results_data.size]
+    )
+    output = helper.make_tensor_value_info("output", TensorProto.FLOAT, [out_bits])
+
     # Define the custom node with "python" mode
-    node_def = helper.make_node(
-        "BinaryTruthTable",
-        ["inputs", "care_set"],
+    custom_node = helper.make_node(
+        "TruthTable",
+        ["in_val", "care_set", "results"],
         ["output"],
         domain="finn.custom_op.general",
         in_bits=in_bits,
+        out_bits=out_bits,
         exec_mode="python",
     )
+
     # Create the graph and the model
-    modelproto = helper.make_model(
-        helper.make_graph([node_def], "test_model", [inputs, care_set], [output])
+    custom_model = helper.make_model(
+        helper.make_graph(
+            [custom_node], "test_model", [in_val, care_set, results], [output]
+        )
     )
     # Wrap the model for finn and set the input tensor datatypes as desired
-    model = ModelWrapper(modelproto)
-    model.set_tensor_datatype("inputs", DataType.BINARY)
-    model.set_tensor_datatype("care_set", DataType.UINT32)
+    finn_model = ModelWrapper(custom_model)
+    finn_model.set_tensor_datatype("in_val", DataType.BINARY)
+    finn_model.set_tensor_datatype("care_set", DataType.UINT32)
+    finn_model.set_tensor_datatype("results", DataType.UINT32)
 
     # test output shape
-    model = model.transform(InferShapes())
-    assert model.get_tensor_shape("output") == [1]
+    finn_model = finn_model.transform(InferShapes())
+    assert finn_model.get_tensor_shape("output") == [out_bits]
+
     # test output type
-    assert model.get_tensor_datatype("output") is DataType.FLOAT32
-    model = model.transform(InferDataTypes())
-    assert model.get_tensor_datatype("output") is DataType.BINARY
+    assert finn_model.get_tensor_datatype("output") is DataType.FLOAT32
+    finn_model = finn_model.transform(InferDataTypes())
+    assert finn_model.get_tensor_datatype("output") is DataType.BINARY
+
     # Give unique names to each node
-    model = model.transform(GiveUniqueNodeNames())
+    finn_model = finn_model.transform(GiveUniqueNodeNames())
 
     # care_set dictionary
     care_set_dict = {
         "care_set": care_set_data,
+        "results": results_data,
     }
 
     # Generate verilog
-    model = model.transform(
-        GenBinaryTruthTableVerilog(num_workers=None, care_set=care_set_dict)
+    finn_model = finn_model.transform(
+        GenTruthTableVerilog(num_workers=None, care_set=care_set_dict)
     )
 
     # Loop over "python" and "rtlsim" execution modes
@@ -113,20 +123,29 @@ def test_binarytruthtable():
         for input_data in input_data_vector:
             # Create input dictionary
             in_dict = {
-                "inputs": input_data,
+                "in_val": input_data,
                 "care_set": care_set_data,
+                "results": results_data,
             }
-            # Perform execution
-            out_dict = oxe.execute_onnx(model, in_dict)
 
-            out_idx = npy_to_rtlsim_input(input_data, DataType.BINARY, in_bits, False)[
+            # Perform execution
+            out_dict = oxe.execute_onnx(finn_model, in_dict)
+            output = out_dict["output"]
+
+            input_int = npy_to_rtlsim_input(
+                input_data, DataType.BINARY, in_bits, False
+            )[0]
+
+            if input_int in care_set_data:
+                index = np.where(care_set_data == input_int)[0][0]
+                pred_output = results_data[index]
+            else:
+                pred_output = 0
+
+            output_dec = npy_to_rtlsim_input(output, DataType.BINARY, out_bits, False)[
                 0
             ]
-
-            entry = 1 if out_idx in care_set_data else 0
-            expected = np.array([entry])
-            # compare outputs
-            assert expected == out_dict["output"]
+            assert output_dec == pred_output
         # Change execution mode into "rtlsim" for simulation with PyVerilator
-        myOp = getCustomOp(model.graph.node[0])
+        myOp = getCustomOp(finn_model.graph.node[0])
         myOp.set_nodeattr("exec_mode", "rtlsim")
