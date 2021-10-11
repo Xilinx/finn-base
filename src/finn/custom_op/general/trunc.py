9e995d1e5b1ee5342b6032bf2f4e871c8fec28dd
Hendrik Borras <hendrikborras@web.de>
2021-10-11 17:42:02 +0100

    Added support for new Trunc op.

--- src/finn/custom_op/general/trunc.py
+++ src/finn/custom_op/general/trunc.py
@@ -31,9 +31,10 @@ import onnx.helper as helper
 
 from finn.core.datatype import DataType
 from finn.custom_op.base import CustomOp
+from finn.custom_op.general.quant import resolve_rounding_mode
 
 
-def trunc(inp_tensor, scale, zeropt, trunc_bit_width):
+def trunc(inp_tensor, scale, zeropt, input_bit_width, output_bit_width, rounding_mode):
     # Port of TruncIntQuant class from Brevitas: https://bit.ly/3wzIpTR
 
     # Scaling
@@ -42,15 +43,14 @@ def trunc(inp_tensor, scale, zeropt, trunc_bit_width):
     # Rounding
     y = np.round(y)
     # Truncate
-    # trunc_scale = 2.0 ** trunc_bit_width
-    # ToDo: Do this proper
-    trunc_scale = 2.0 ** 6
+    trunc_bit_width = input_bit_width - output_bit_width
+    trunc_scale = 2.0 ** trunc_bit_width
     y = y / trunc_scale
 
     # To int
     # ToDo: Check if this is correct
-    # y = self.float_to_int_impl(y)
-    y = y.astype(np.int64).astype(np.float32)
+    rounding_fx = resolve_rounding_mode(rounding_mode)
+    y = rounding_fx(y)
 
     # Rescale
     y = y - zeropt
@@ -74,7 +74,10 @@ class Trunc(CustomOp):
         return {
             # whether the quantization interval should be signed or not
             # (e.g. at 8b unsigned=[0, 255] vs signed=[-128, 127])
+            # ToDo: Is the signed attribute really needed?
             "signed": ("i", True, 1),
+            # The rounding mode, which is used for the trunc function
+            "rounding_mode": ("s", True, "FLOOR"),
         }
 
     def make_shape_compatible_op(self, model):
@@ -82,6 +85,7 @@ class Trunc(CustomOp):
         return helper.make_node("Identity", [node.input[0]], [node.output[0]])
 
     def get_trunc_config(self, model):
+        # ToDo: Get this to work properly
         node = self.onnx_node
         signed = self.get_nodeattr("signed")
         # scale, zero-point and bitwidth must be read from initializers
@@ -124,9 +128,14 @@ class Trunc(CustomOp):
         inp_tensor = context[node.input[0]]
         scale = context[node.input[1]]
         zeropt = context[node.input[2]]
-        trunc_bit_width = context[node.input[3]]
+        input_bit_width = context[node.input[3]]
+        output_bit_width = context[node.input[4]]
+        # save attributes
+        rounding_mode = self.get_nodeattr("rounding_mode")
         # calculate output
-        ret = trunc(inp_tensor, scale, zeropt, trunc_bit_width)
+        ret = trunc(
+            inp_tensor, scale, zeropt, input_bit_width, output_bit_width, rounding_mode
+        )
         # set context according to output name
         context[node.output[0]] = ret
 
