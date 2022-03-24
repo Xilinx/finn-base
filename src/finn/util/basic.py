c8830e846ed9a960aff191386e19d48574df752c
Felix Jentzsch <45395194+fpjentzsch@users.noreply.github.com>
2022-03-24 15:50:04 +0100

    Add RFSoC 2x2 board to part map (#59)

--- src/finn/util/basic.py
+++ src/finn/util/basic.py
@@ -45,6 +45,7 @@ pynq_part_map["Pynq-Z2"] = "xc7z020clg400-1"
 pynq_part_map["ZCU102"] = "xczu9eg-ffvb1156-2-e"
 pynq_part_map["ZCU104"] = "xczu7ev-ffvc1156-2-e"
 pynq_part_map["ZCU111"] = "xczu28dr-ffvg1517-2-e"
+pynq_part_map["RFSoC2x2"] = "xczu28dr-ffvg1517-2-e"
 
 # native AXI HP port width (in bits) for PYNQ boards
 pynq_native_port_width = dict()
@@ -54,6 +55,7 @@ pynq_native_port_width["Ultra96"] = 128
 pynq_native_port_width["ZCU102"] = 128
 pynq_native_port_width["ZCU104"] = 128
 pynq_native_port_width["ZCU111"] = 128
+pynq_native_port_width["RFSoC2x2"] = 128
 
 # Alveo device and platform mappings
 alveo_part_map = dict()
